b74a4bdd4424050ae1b77f653d86c3bb6e786ddd
Kleczyk <mailnasyff@gmail.com>
2024-06-01 18:13:12 +0200

    firx Embedings.py

--- src/Embeddings.py
+++ src/Embeddings.py
@@ -13,11 +13,4 @@ def text2vec(text):
         response_data = response.json()
         embeddings_data = response_data['data']
 
-        print(embeddings_data)
-
-    return embeddings_data[0]["embedding"]
-
-
-vec = text2vec("This is a document about pineapple")
-print(vec)
-print(len(vec))
+        return embeddings_data[0]["embedding"]
